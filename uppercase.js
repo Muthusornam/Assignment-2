f3623455df02f92065c78b0dacdc6f4e91dd5e8a
Muthusornam <94225877+Muthusornam@users.noreply.github.com>
2021-11-28 13:00:17 +0530

    Update uppercase.js

--- uppercase.js
+++ uppercase.js
@@ -1,7 +1,9 @@
-function upper(str){
+function upper(arr){
 	var a=[];
-	var c=str.toUpperCase();
+	for(i=0;i<arr.length;i++){
+	var c=arr[i].toUpperCase();
 	a.push(c);
+	}
 	return a;
 }
-console.log(upper("hello"));
+console.log(upper(["hello","welcome","on","Board"]));
